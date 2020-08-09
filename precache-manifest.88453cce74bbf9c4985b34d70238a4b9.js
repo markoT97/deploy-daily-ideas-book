08209296d8d68d5b6e359b6a3adc21b48ac26935
Marko Tričković <marko.trickovic13@gmail.com>
2020-08-10 00:16:58 +0200

    Updates

---
+++ precache-manifest.88453cce74bbf9c4985b34d70238a4b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfaf2b10d85b620f2f693633255ec2fb",
+    "url": "/deploy-daily-ideas-book/index.html"
+  },
+  {
+    "revision": "006fb897456efd9f6b25",
+    "url": "/deploy-daily-ideas-book/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "3968557fb8ea6a24dd06",
+    "url": "/deploy-daily-ideas-book/static/css/main.f0546147.chunk.css"
+  },
+  {
+    "revision": "006fb897456efd9f6b25",
+    "url": "/deploy-daily-ideas-book/static/js/2.20b7a480.chunk.js"
+  },
+  {
+    "revision": "f1b8bfc3915e9dbfa89fdc50283a6b25",
+    "url": "/deploy-daily-ideas-book/static/js/2.20b7a480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3968557fb8ea6a24dd06",
+    "url": "/deploy-daily-ideas-book/static/js/main.590680c6.chunk.js"
+  },
+  {
+    "revision": "52aa8df0bb59eb73a8a3",
+    "url": "/deploy-daily-ideas-book/static/js/runtime-main.f9c1c8a6.js"
+  }
+]);
